27e8e92e6cd1f203132b4786ddcb9d5b34ff5284
leonseehaojun <leonsee1000@yahoo.com.sg>
2025-10-24 17:01:29 +0800

    Change Role Names

--- src/data/team.ts
+++ src/data/team.ts
@@ -3,15 +3,15 @@ export type TeamMember = {
   id: string;
   name: string;
   role: string;
-  photo: string;   // put images in /public/team/
+  photo: string;  
   bio?: string;
   linkedin?: string;
 };
 
 const team: TeamMember[] = [
   { id: "t-1", name: "Zen Tay",  role: "Founder & Director",  photo: "/zen.jpeg" },
-  { id: "t-2", name: "Maya Lee",  role: "Producer",            photo: "/team/maya.jpg" },
-  { id: "t-3", name: "Rio Chen",  role: "Cinematographer",     photo: "/team/rio.jpg" },
+  { id: "t-2", name: "Valda Lim",  role: "Intern",            photo: "/team/maya.jpg" },
+  { id: "t-3", name: "Ferlyn",  role: "Intern",     photo: "/team/rio.jpg" },
 ];
 
 export default team;
